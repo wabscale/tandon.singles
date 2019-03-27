ec80964877ae503020042e6e2cb4f9933f2a75ab
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2019-03-27 19:38:47 -0400

    fix forms

--- web/home/forms.py
+++ web/home/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField, FileField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, InputRequired
 
 
 class PostForm(FlaskForm):
     image = FileField('Image', validators=[DataRequired()])
-    caption = StringField('Caption', validators=[DataRequired()])
+    caption = StringField('Caption', validators=[InputRequired()])
     public = BooleanField('Public post')
     post=SubmitField('Post', )

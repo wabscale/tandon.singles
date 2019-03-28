4457dd99dd28349b0fef1bdd8154b277005a8d21
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2019-03-28 09:40:18 -0400

    fix a typo

--- web/api/routes.py
+++ web/api/routes.py
@@ -2,8 +2,6 @@ from flask import request, redirect, url_for, flash, render_template, Blueprint
 from flask_login import current_user, login_user, logout_user, login_required
 import pymysql
 
-from .forms import PostForm
-
 from ..app import app
 from ..models import User
 

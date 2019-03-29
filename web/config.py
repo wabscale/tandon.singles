a6ed8fc7e5214ea1d149ce34f509e3127cdaed94
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2019-03-28 20:37:32 -0400

    add upload and log dir config

--- web/config.py
+++ web/config.py
@@ -13,9 +13,12 @@ class Config:
     MYSQL_DATABASE_HOST = 'db'
     MYSQL_DATABASE_DB = 'TS'
 
-    UPLOAD_FOLDER = 'web/.data/uploads'
+    UPLOAD_DIR = 'web/.data/uploads'
+    LOG_DIR = '.data/log'
 
     def __init__(self):
+        os.makedirs(self.UPLOAD_DIR, exist_ok=True)
+        os.makedirs(self.LOG_DIR, exist_ok=True)
         if any('dev.py' in arg for arg in sys.argv):
             self.SECRET_KEY = 'DEBUG'
             self.MYSQL_DATABASE_HOST = 'localhost'

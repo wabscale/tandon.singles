5fb9d7ea93003c7f0f5aed50e83f114db180343d
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2019-03-31 16:46:15 -0400

    add logging

--- web/app.py
+++ web/app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template, request, redirect, g, Blueprint, send_from_directory
-from flask_login import current_user, login_required
 from flask_bootstrap import Bootstrap
 from flask_wtf import CSRFProtect
 from flaskext.mysql import MySQL
+from datetime import datetime
+from flask import Flask
+import logging
+import os
 
 from .config import Config
 
 host = '0.0.0.0'
 port = 5000
+start_time = datetime.now()
 
 app = Flask(__name__, static_url_path='/static')
 app.config.from_object(Config())
@@ -16,17 +19,28 @@ Bootstrap(app)
 CSRFProtect(app)
 db = MySQL(app)
 
+if app.config['VERBOSE_SQL_GENERATION']:
+    print(os.path.join(
+        os.getcwd(),
+        app.config['LOG_DIR'],
+        'orm_log.log'
+    ))
+    logging.basicConfig(filename=os.path.join(
+        app.config['LOG_DIR'],
+        'orm_log.log'
+    ), filemode='w+')
+
+
+
 # register blueprints
 from .auth import auth
 from .users import users
 from .home import home
-from .api import api
 
 list(map(app.register_blueprint, (
     home,
     auth,
-    users,
-    api
+    users
 )))
 
 

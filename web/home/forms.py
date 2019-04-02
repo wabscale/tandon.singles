09fc5b2ca7ba145122c5b7a6b249a5d63e84b711
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2019-04-02 19:39:17 -0400

    pretty home

--- web/home/forms.py
+++ web/home/forms.py
@@ -1,10 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField, FileField, BooleanField
 from wtforms.validators import DataRequired, InputRequired
+from wtforms.widgets import TextArea
 
 
 class PostForm(FlaskForm):
-    image = FileField('Image', validators=[DataRequired()])
-    caption = StringField('Caption', validators=[InputRequired()])
-    public = BooleanField('Public post')
+    image = FileField(
+        'Image',
+        validators=[DataRequired()]
+    )
+    caption = StringField(
+        'Caption',
+        widget=TextArea(),
+        validators=[InputRequired()]
+    )
+    public = BooleanField(
+        'Public post',
+        default=True
+    )
+
     post=SubmitField('Post', )

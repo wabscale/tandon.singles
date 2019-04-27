6a67cccefb1333dab52b7f237fb729459ee33492
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2019-04-26 21:28:22 -0400

    add checks

--- web/photos/routes.py
+++ web/photos/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_required, current_user
 
 from ..app import db
 
@@ -6,6 +7,7 @@ photos=Blueprint('photos', __name__, url_prefix='/p')
 
 
 @photos.route('/<photoID>')
+@login_required
 def view(photoID):
     p=db.query('Photo').find(
         photoID=photoID,
@@ -13,6 +15,24 @@ def view(photoID):
 
     if p is None:
         return redirect(url_for('home.index'))
+
+    follows=db.query('Follow').find(
+        followerUsername=current_user.username,
+        followeeUsername=p.PhotoOwner
+    ).first()
+
+    if follows is None or not follows.acceptedfollow or not p.allFollowers:
+        return redirect(url_for('home.index'))
+
+    shares = list(p.shares)
+
+    if len(shares) != 0:
+        share=shares[0]
+        if all(
+            share.groupName != group.groupName and share.groupOwner != group.groupName
+            for group in current_user.closefriendgroups
+        ):
+            return redirect('home.index')
 
     return render_template(
         'photos/view.html',
